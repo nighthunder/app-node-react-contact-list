614aa58b2e5b67dff43f39fc482c13fc5f57c8aa
mayara-morais <57415644+mayara-morais@users.noreply.github.com>
2022-03-29 20:11:08 -0300

    Node get routes

--- node/index.js
+++ node/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 
@@ -22,25 +22,63 @@ app.get("/", async (req, res) => {
     res.json({status: "Minhau! Ready to slay"})
 })
 
-app.get("/:id", async (req, res) => {
-    const query = "Select * from contact_list WHERE id = ?";
+app.get("/contacts/", async (req, res) => {
+    const query = "Select * from contact_list";
     pool.query(query, [req.params.name], (error, results) =>{
-        /*if (!results[0]){
+        if (!results[0]){
             res.json({status: "Not found!"});
+            //res.end("oi");
         }else{
-            res.json(results[0])
-        }*/
+            res.json(results)
+            //res.end("no");
+        }
 
-        if (error)
-            console.log('Connection error: ', error);
+        if (error) {console.log('Connection error: ', error)}
+    })
+})
+
+app.get("/contacts/:id", async (req, res) => {
+    const query = "Select * from contact_list";
+    pool.query(query, [req.params.id], (error, results) =>{
+        if (!results[0]){
+            res.json({status: "Not found!"});
+            //res.end("oi");
+        }else{
+            res.json(results)
+            //res.end("no");
+        }
+
+        if (error) {console.log('Connection error: ', error)}
+    })
+})
+
+app.post("/contacts/add/", async (req, res) => {
+    const user_name = String(req.body.name);
+    const user_email = String(req.body.email);  
+    const user_phone = String(req.body.phone);
+    const contact = req.body;
+    console.log("BODIEESSS", req.body);
+    const query = "Insert into contact_list (id, `name`, email, phone) VALUES(DEFAULT,?,?,?)";
+    pool.query(query, [user_name, user_email, user_phone], (error, results) =>{
+        if (error) {
+            console.log('Connection error: ', error)}
+        else{
+            if (!results){
+                res.json({status: "Not found!"});
+                //res.end("oi");
+            }else{
+                res.json({status: "Contato inserido"})
+                //res.end("no");
+            }    
+        }
     })
 })
 
 const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    socketPath: `/clousql/${process.env.INSTANCE_CONNECTION_NAME}`
-    
 })
 
